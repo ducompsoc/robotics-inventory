050687d0f07dda25ebbbc6acfdf5cb853e2ef7d3
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-08-31 21:25:15 +0100

    Style logout page

--- src/app/signed-out/page.tsx
+++ src/app/signed-out/page.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 export default function SignedOut() {
   return (
-    <main className="p-8">
-      <h1>Signed out</h1>
-      <p>You have been signed out of Robotics Inventory and Keycloak.</p>
-      <Link href="/">Sign in again</Link>
+    <main className="p-10 flex-1 flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center text-center gap-5 border border-background-warm rounded-xl px-5 py-10 w-sm text-balance">
+        <h1 className="text-2xl">Signed out</h1>
+        <p>You have been signed out of Robotics Inventory.</p>
+        <Link className="underline" href="/">
+          Sign in again
+        </Link>
+      </div>
     </main>
   );
 }
