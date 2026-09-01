022d76efb82e5e6fc3ef473b5a92229d53a18b3c
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-09-01 20:48:58 +0100

    Create index

---
+++ src/lib/inventory/index.ts
@@ -0,0 +1,6 @@
+export * from "@/lib/inventory/contacts";
+export * from "@/lib/inventory/errors";
+export * from "@/lib/inventory/items";
+export * from "@/lib/inventory/loans";
+export * from "@/lib/inventory/teams";
+export * from "@/lib/inventory/types";
