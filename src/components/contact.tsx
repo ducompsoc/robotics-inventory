c354007955162f6603e80f88eb49dcda3b78ae32
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-09-01 21:51:08 +0100

    Fix contact resizing with id card icon

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -23,14 +23,14 @@ export default function Contact({ email, idHeld, name }: ContactProps) {
     <div className="flex flex-col text-sm p-2">
       <button
         type="button"
-        className={`font-bold text-left cursor-pointer disabled:cursor-wait disabled:opacity-50 ${
+        className={`flex items-center gap-1 font-bold text-left cursor-pointer disabled:opacity-50 ${
           isPending ? "opacity-50 cursor-wait" : ""
         }`}
         disabled={isPending}
         onClick={handleToggleIdHeld}
       >
-        {name}{" "}
-        {idHeld ? <IdCard className="inline-block h-lh text-contrast" /> : null}
+        <span>{name}</span>
+        {idHeld ? <IdCard className="h-lh w-lh text-contrast" /> : null}
       </button>
       <div>{email}</div>
     </div>
